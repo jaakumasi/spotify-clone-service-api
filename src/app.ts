94c6db57434f7b51223dacab1418abd7a03efd2a
jaakumasi <jeromeakumasi01@gmail.com>
2022-11-16 15:15:39 +0000

    tested all routers

--- src/app.ts
+++ src/app.ts
@@ -17,6 +17,6 @@ app.use('/api/v1/users', usersRouter);
 app.use('/api/v1/login', authRouter);
 app.use('/api/v1/songs', songsRouter);
 app.use('/api/v1/playlists', playlistRouter);
-app.use('/api/v1/search', searchRouter);
+app.use('/api/v1/', searchRouter);
 
 app.listen(process.env.PORT, () => console.log(`server running port ${process.env.PORT}...`));
--- src/routes/search.ts
+++ src/routes/search.ts
@@ -1,12 +1,23 @@
 import * as express from 'express';
 import { validate, PlaylistModel } from '../models/playlist';
-import { UserModel } from '../models/user';
-import { admin, auth, validateObjectID } from '../middleware';
+import { auth, validateObjectID } from '../middleware';
 import * as joi from 'joi';
 import { SongModel } from '../models/song';
 /**
  * router to handle search
  */
 const searchRouter = express.Router();
+
+searchRouter.post('/', auth, async (req: express.Request, res: express.Response) => {
+    const search = req.query.search;
+    if (search !== '') {
+        const songs = await SongModel.find({ name: { $regex: search, $options: 'i' } }).select('-__v').limit(10);
+        const playlists = await PlaylistModel.find({ name: { $regex: search, $options: 'i' } }).select('-__v').limit(10);
+        return res.status(200).send({ data: { playlists, songs } });
+    }
+    else {
+        return res.status(200).send({});
+    }
+})
 
 export default searchRouter;
